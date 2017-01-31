600cebec3689890fd1f174f50fbdc1eb528ea0c8
AurelienCaille <aur-lien@hotmail.fr>
2017-01-31 14:48:21 +0100

    Update and rename Pion.py to Pawn.py

---
+++ Pawn.py
@@ -0,0 +1,10 @@
+class Pawn(object):
+    """ Class representating a pawn of the game """
+
+    def __init__(self, color):
+
+        self.color = color
+
+    def change_color(self, color):
+        """ Change the color of the pawn to the new color """
+        self.color = color
