5edf1025d72aba0d05b80641f231beb928296453
AurelienCaille <aur-lien@hotmail.fr>
2017-01-31 14:44:15 +0100

    First complete of virusgame class

--- VirusGame.py
+++ VirusGame.py
@@ -1,2 +1,12 @@
+from Grid import Grid
+
 class VirusGame(object):
-    pass
+
+    def __init__(self, size, first_player):
+
+        self.grid = Grid(size)
+        self.first_player = first_player
+
+    def play(self):
+        pass
+
