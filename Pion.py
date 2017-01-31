02483637e943aacd068cfba9cfd6d10a17b0c716
AurelienCaille <aur-lien@hotmail.fr>
2017-01-31 14:39:36 +0100

    Complete pion class

--- Pion.py
+++ Pion.py
@@ -1,2 +1,10 @@
 class Pion(object):
-    pass
+    """ Class representating a pion of the game """
+
+    def __init__(self, color):
+
+        self.color = color
+
+    def change_color(self, color):
+        """ Change the color of the pion to the new color """
+        self.color = color
