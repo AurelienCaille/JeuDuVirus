21fdd86ece055968b5be5c466656ec63ea5c67e5
AurelienCaille <aur-lien@hotmail.fr>
2017-01-31 14:39:59 +0100

    Complete player class

--- Player.py
+++ Player.py
@@ -1,2 +1,13 @@
 class Player(object):
-    pass
+    def __init__(self, name, color):
+
+        self.name = name
+        self.color = color
+        self.point = 0
+        self.adversary = None
+
+    def get_adversary(self):
+        return self.adversary
+    
+    def set_adversary(self, adversary):
+        self.adversary = adversary
